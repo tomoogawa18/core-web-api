e6824f375656c2ee79019c283e17d1802a2ca29f
小川智裕 <tomo.ogawa18@gmail..com>
2020-12-07 21:53:20 +0900

    MySQLの設定

---
+++ Data/SampleMVCAppContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SampleMVCApp.Models;
+
+    public class SampleMVCAppContext : DbContext
+    {
+        public SampleMVCAppContext (DbContextOptions<SampleMVCAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SampleMVCApp.Models.Person> Person { get; set; }
+    }
---
+++ Migrations/20201204082535_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace SampleMVCApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Mail = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.PersonId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
