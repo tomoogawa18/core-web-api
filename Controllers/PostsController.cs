31e2aa5be3be4f0e89839c113a8e145a872f73e5
小川智裕 <tomo.ogawa18@gmail..com>
2020-12-08 16:15:31 +0900

    delete,edit修正

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -44,5 +44,104 @@ namespace SampleMVCApp.Controllers
             return View(post);
         }
 
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null){
+                return NotFound();
+            }
+
+            var post = await _context.Post
+                .FirstOrDefaultAsync(m => m.PostId == id);
+
+            if(post == null){
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        public async Task<IActionResult> Edit(int? id){
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Post.FindAsync(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PostId, Subject, Content")] Post post)
+        {
+            if(id != post.PostId)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                try{
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+
+                }catch(DbUpdateConcurrencyException){
+                    if(!PostExists(post.PostId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(post);
+        }
+
+        private bool PostExists(int id)
+        {
+            return _context.Post.Any(e => e.PostId == id);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Post
+                .FirstOrDefaultAsync(m => m.PostId == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var post = await _context.Post.FindAsync(id);
+            _context.Post.Remove(post);
+
+            await _context.SaveChangesAsync();
+            
+            return RedirectToAction(nameof(Index));
+
+        }
     }
 }
