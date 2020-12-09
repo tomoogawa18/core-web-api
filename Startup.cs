2ec6c5b3bd59e258a916929e9018c6f41cb87e27
小川智裕 <tomo.ogawa18@gmail..com>
2020-12-09 18:32:02 +0900

    Vueの設定

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using SampleMVCApp.VueCoreConnections;
 
 namespace SampleMVCApp
 {
@@ -35,7 +36,7 @@ namespace SampleMVCApp
             services.AddDbContext<SampleMVCAppContext>(options =>
                     options.UseMySQL(Configuration.GetConnectionString("MySQL")));
 
-
+            services.AddSpaStaticFiles(options => options.RootPath = "client-app/dist");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +53,16 @@ namespace SampleMVCApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "client-app";
+                if (env.IsDevelopment())
+                {
+                    spa.UseVueDevelopmentServer();
+                }
             });
         }
     }
